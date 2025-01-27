2298b0f709497e4a95b9b5d8d5202ecab3cb62ee
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2025-01-26 22:20:48 -0800

    testomg

--- server.js
+++ server.js
@@ -28,7 +28,12 @@ const serviceAccount = {
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
     universe_domain: "googleapis.com"
-  };
+};
+
+console.log("please work: ", process.env.FIREBASE_PRIVATE_KEY);
+
+
+console.log("please work test: ", process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'));
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
