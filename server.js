7aefbc966cb788a24d2c42130b97b941a4b10b5e
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2025-01-26 21:39:37 -0800

    modify the server config so it uses environment variable for the sensative data

--- server.js
+++ server.js
@@ -12,11 +12,21 @@ app.use(cors({
     origin: 'https://www.idadog.com',  // replace with your GitHub Pages domain
 }));
 
-
 const PORT = process.env.PORT || 5000;
 
-// Replace './path/to/serviceAccountKey.json' with the path to your downloaded key
-const serviceAccount = require('./config/idadogwebsite-firebase-adminsdk-fbsvc-bc91c86757.json');
+const serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID, 
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // To correctly handle newlines
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+    universe_domain: "googleapis.com"
+  };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
